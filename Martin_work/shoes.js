fb7d5f562ff0c30b3b4d9e94da441e2f73ab2dfc
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-05-12 18:34:30 +0300

    Added js file
    Ooopsi, NOW I added a js file (shoes.js) ;)

---
+++ Martin_work/shoes.js
@@ -0,0 +1,15 @@
+let image_shoes_1 = document.getElementById('image_shoes_1');
+
+let right_arrow = document.getElementById('right_arrow');
+
+let left_arrow = document.getElementById('left_arrow');
+
+function hoverImage() {
+    right_arrow.style.display = 'block';
+    left_arrow.style.display = 'block';
+}
+
+function unhoverImage() {
+    right_arrow.style.display = 'none';
+    left_arrow.style.display = 'none';
+}
