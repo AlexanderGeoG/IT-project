fbb7e1548ad21df682572f304d00e68f69cfd7ff
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-05-12 21:14:06 +0300

    Made the arrows work
    Now the arrows change the color of the product

--- Martin_work/shoes.js
+++ Martin_work/shoes.js
@@ -4,6 +4,9 @@ let right_arrow = document.getElementById('right_arrow');
 
 let left_arrow = document.getElementById('left_arrow');
 
+let shoes_images = ['shoes_11.jpg', 'shoes_12.jpg', 'shoes_13.jpg'];
+let currentImage = 0;
+
 function hoverImage() {
     right_arrow.style.display = 'block';
     left_arrow.style.display = 'block';
@@ -12,4 +15,20 @@ function hoverImage() {
 function unhoverImage() {
     right_arrow.style.display = 'none';
     left_arrow.style.display = 'none';
+}
+
+function onRightArrowClick() {
+    currentImage++;
+    if(currentImage > 2) {
+        currentImage = 0;
+    }
+    document.getElementById('image_shoes_1').src = shoes_images[currentImage];
+}   
+
+function onLeftArrowClick() {
+    currentImage--;
+    if(currentImage < 0) {
+        currentImage = 2;
+    }
+    document.getElementById('image_shoes_1').src = shoes_images[currentImage];
 }
