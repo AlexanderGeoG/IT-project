b6552da50837faec8d5a2883a31e58f699ac2c09
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-05-16 17:19:38 +0300

    Added second offer for shoes, and changed the path of the images

--- Martin_work/shoes.js
+++ Martin_work/shoes.js
@@ -4,20 +4,20 @@ let right_arrow = document.getElementById('right_arrow');
 
 let left_arrow = document.getElementById('left_arrow');
 
-let shoes_images = ['shoes_11.jpg', 'shoes_12.jpg', 'shoes_13.jpg'];
+let shoes_images = ['../Images/shoes_11.jpg', '../Images/shoes_12.jpg', '../Images/shoes_13.jpg'];
 let currentImage = 0;
 
-function hoverImage() {
+function hoverImage1() {
     right_arrow.style.display = 'block';
     left_arrow.style.display = 'block';
 }
 
-function unhoverImage() {
+function unhoverImage1() {
     right_arrow.style.display = 'none';
     left_arrow.style.display = 'none';
 }
 
-function onRightArrowClick() {
+function onRightArrowClick1() {
     currentImage++;
     if(currentImage > 2) {
         currentImage = 0;
@@ -25,10 +25,48 @@ function onRightArrowClick() {
     document.getElementById('image_shoes_1').src = shoes_images[currentImage];
 }   
 
-function onLeftArrowClick() {
+function onLeftArrowClick1() {
     currentImage--;
     if(currentImage < 0) {
         currentImage = 2;
     }
     document.getElementById('image_shoes_1').src = shoes_images[currentImage];
+}
+
+//second offer
+
+
+let image_shoes_2 = document.getElementById('image_shoes_2');
+
+let right_arrow2 = document.getElementById('right_arrow2');
+
+let left_arrow2 = document.getElementById('left_arrow2');
+
+let shoes_images2 = ['../Images/shoes_21.jpg ', '../Images/shoes_22.jpg', '../Images/shoes_23.jpg'];
+let currentImage2 = 0;
+
+function hoverImage2() {
+    right_arrow2.style.display = 'block';
+    left_arrow2.style.display = 'block';
+}
+
+function unhoverImage2() {
+    right_arrow2.style.display = 'none';
+    left_arrow2.style.display = 'none';
+}
+
+function onRightArrowClick2() {
+    currentImage2++;
+    if(currentImage2 > 2) {
+        currentImage2 = 0;
+    }
+    document.getElementById('image_shoes_2').src = shoes_images2[currentImage2];
+}   
+
+function onLeftArrowClick2() {
+    currentImage2--;
+    if(currentImage2 < 0) {
+        currentImage2 = 2;
+    }
+    document.getElementById('image_shoes_2').src = shoes_images2[currentImage2];
 }
