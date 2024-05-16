c9b5228fdb7188190d4d63afc7d76df0484cf3dc
Dido-G <dgolosmanov@gmail.com>
2024-05-16 16:21:44 +0300

    Made new page and corrected home images
    changed adresses of the home images and made all functionality for the first offer of the sunglasses page

---
+++ Dido_work/sunglasses.js
@@ -0,0 +1,34 @@
+let image_shoes_1 = document.getElementById('image_sunglasses_1');
+
+let right_arrow = document.getElementById('right_arrow');
+
+let left_arrow = document.getElementById('left_arrow');
+
+function hoverImage() {
+    right_arrow.style.display = 'block';
+    left_arrow.style.display = 'block';
+}
+
+function unhoverImage() {
+    right_arrow.style.display = 'none';
+    left_arrow.style.display = 'none';
+}
+
+let currentImage = 0;
+let images = ["sunglasses1.1.png", "sunglasses1.2.png", "sunglasses1.3.png"];
+
+function RigthArrow(){
+    currentImage++;
+    if(currentImage > 2){
+        currentImage = 0;
+    }
+    document.getElementById("image_sunglasses_1").src = images[currentImage];
+}
+
+function LeftArrow(){
+    currentImage--;
+    if(currentImage < 0){
+        currentImage = 2;
+    }
+    document.getElementById("image_sunglasses_1").src = images[currentImage];
+}
