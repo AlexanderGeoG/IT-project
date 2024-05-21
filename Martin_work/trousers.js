a7d4c71b2bd7f1bb4235966ca50364228a363eac
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-05-21 20:32:41 +0300

    Added colors to the trousers
    The color of the trousers is displayed in the shopping cart when the products is bought

--- Martin_work/trousers.js
+++ Martin_work/trousers.js
@@ -1,11 +1,13 @@
+//fisrt offer
 let image_tr_1 = document.getElementById('image_tr_1');
 
 let right_arrow = document.getElementById('right_arrow');
 
 let left_arrow = document.getElementById('left_arrow');
 
-let trs_images = ['../Images/trousers_11.jpg', '../Images/trousers_12.jpg', '../Images/trousers_13.jpg'];
-let currentImage = 0;
+let trs_images1 = ['../Images/trousers_11.jpg', '../Images/trousers_12.jpg', '../Images/trousers_13.jpg'];
+let trs_color1 = ['Dark Red', 'Grey', 'White'];
+let currentImage1 = 0;
 
 function hoverImage1() {
     right_arrow.style.display = 'block';
@@ -18,19 +20,26 @@ function unhoverImage1() {
 }
 
 function onRightArrowClick1() {
-    currentImage++;
-    if(currentImage > 2) {
-        currentImage = 0;
+    currentImage1++;
+    if(currentImage1 > 2) {
+        currentImage1 = 0;
     }
-    document.getElementById('image_tr_1').src = trs_images[currentImage];
+    document.getElementById('image_tr_1').src = trs_images1[currentImage1];
+
+    
+    let currentAlt = "Mens Fashion Athletic Joggers Pants";
+    document.getElementById('image_tr_1').setAttribute('alt', currentAlt + ' ' + trs_color1[currentImage1]);
 }   
 
 function onLeftArrowClick1() {
-    currentImage--;
-    if(currentImage < 0) {
-        currentImage = 2;
+    currentImage1--;
+    if(currentImage1 < 0) {
+        currentImage1 = 2;
     }
-    document.getElementById('image_tr_1').src = trs_images[currentImage];
+    document.getElementById('image_tr_1').src = trs_images1[currentImage1];
+
+    let currentAlt = "Mens Fashion Athletic Joggers Pants Dark";
+    document.getElementById('image_tr_1').setAttribute('alt', currentAlt + ' ' + trs_color1[currentImage1]);
 }
 
 
@@ -45,6 +54,7 @@ let right_arrow2 = document.getElementById('right_arrow2');
 let left_arrow2 = document.getElementById('left_arrow2');
 
 let tr_images2 = ['../Images/trousers_21.jpg', '../Images/trousers_22.jpg', '../Images/trousers_23.jpg'];
+let trs_color2 = ['Light Green', 'Black', 'Grey'];
 let currentImage2 = 0;
 
 function hoverImage2() {
@@ -63,6 +73,9 @@ function onRightArrowClick2() {
         currentImage2 = 0;
     }
     document.getElementById('image_tr_2').src = tr_images2[currentImage2];
+
+    let currentAlt = "Essentials Men's Classic-Fit Wrinkle-Resistant Flat-Front Chino Pants";
+    document.getElementById('image_tr_2').setAttribute('alt', currentAlt + ' ' + trs_color2[currentImage2]);
 }   
 
 function onLeftArrowClick2() {
@@ -71,6 +84,9 @@ function onLeftArrowClick2() {
         currentImage2 = 2;
     }
     document.getElementById('image_tr_2').src = tr_images2[currentImage2];
+
+    let currentAlt = "Essentials Men's Classic-Fit Wrinkle-Resistant Flat-Front Chino Pants";
+    document.getElementById('image_tr_2').setAttribute('alt', currentAlt + ' ' + trs_color2[currentImage2]);
 }
 
 
@@ -84,6 +100,7 @@ let right_arrow3 = document.getElementById('right_arrow3');
 let left_arrow3 = document.getElementById('left_arrow3');
 
 let tr_images3 = ['../Images/trousers_31.jpg', '../Images/trousers_32.jpg', '../Images/trousers_33.jpg'];
+let trs_color3 = ['Blue', 'White', 'Dark Red'];
 let currentImage3 = 0;
 
 function hoverImage3() {
@@ -102,6 +119,9 @@ function onRightArrowClick3() {
         currentImage3 = 0;
     }
     document.getElementById('image_tr_3').src = tr_images3[currentImage3];
+
+    let currentAlt = "Men's Casual Drawstring Pants Regular Fit Elastic Waist Cotton Basic Straight Leg Cargo Pants";
+    document.getElementById('image_tr_3').setAttribute('alt', currentAlt + ' ' + trs_color3[currentImage3]);
 }   
 
 function onLeftArrowClick3() {
@@ -110,6 +130,9 @@ function onLeftArrowClick3() {
         currentImage3 = 2;
     }
     document.getElementById('image_tr_3').src = tr_images3[currentImage3];
+
+    let currentAlt = "Men's Casual Drawstring Pants Regular Fit Elastic Waist Cotton Basic Straight Leg Cargo Pants";
+    document.getElementById('image_tr_3').setAttribute('alt', currentAlt + ' ' + trs_color3[currentImage3]);
 }
 
 function AddProduct(){
