2ed4faa8f15226412c2f898db575eab8dfba61fe
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-05-24 11:26:33 +0300

    Added product message
    When the product is added to the cart, the button of the product change its text to show the user that the product has been added to the cart

--- Dido_work/function_addToCart.js
+++ Dido_work/function_addToCart.js
@@ -1,15 +1,13 @@
 // add a product to the cart
-function AddtoCart(product, priceId) {
+function AddtoCart(product, priceId, button_id) {
     let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
     cart.push({name: product.alt, image: product.src, price: document.getElementById(priceId).textContent});
     localStorage.setItem('cart', JSON.stringify(cart));
 
 
-    let myh1 = document.createElement("h1");
-    myh1.textContent = "Added to cart";
-    document.body.appendChild(myh1);
+    document.getElementById(button_id).innerHTML = 'Added product';
 
     setTimeout(() => {
-        myh1.remove();
-    }, 3000)
+        document.getElementById(button_id).innerHTML = 'Add to cart';
+    }, 1000)
 }
